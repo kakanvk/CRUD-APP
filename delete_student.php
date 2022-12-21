4c427134b82aa92e32fa395dbc739f693ea82b85
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2022-12-21 13:26:53 +0700

    .

---
+++ delete_student.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_POST['id'])) {
+	$id = $_POST['id'];
+
+	require_once ('dbhelp.php');
+	$sql = 'delete from student where id = '.$id;
+	execute($sql);
+
+	echo 'Xoá sinh viên thành công';
+}
